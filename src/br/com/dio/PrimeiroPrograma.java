4872f82daba49d1565f00053b80c0d4656b9cbfe
Paloma <palomachaveiro777@gmail.com>
2022-03-16 13:25:46 -0300

    Seccond commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,11 +7,45 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
-
       /*  int a = 5;
         int b = 3;
 
         System.out.println("Hello World! " + (a+b));*/
     }
-}
 
+    class livro {
+
+        private String nome;
+        private Integer numPáginas;
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPáginas() {
+            return numPáginas;
+        }
+
+        public livro(String nome, Integer numPáginas) {
+            this.nome = nome;
+            this.numPáginas = numPáginas;
+        }
+
+        public void setNumPáginas(Integer numPáginas) {
+            this.numPáginas = numPáginas;
+        }
+
+        @Override
+        public String toString() {
+            return "livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPáginas=" + numPáginas +
+                    '}';
+
+        }
+    }
+}
